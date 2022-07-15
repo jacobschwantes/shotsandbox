728ca12dbeea2ee005c89cf092a4e45e39f46b17
skrt-dev <jacobschwantes@outlook.com>
2022-07-15 05:33:42 -0500

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/common/components/**/*.{js,ts,jsx,tsx}",
+    "./src/common/layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
