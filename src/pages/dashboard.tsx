4031a657fc051d1b8c3e358f84fefba6231b04b3
skrt-dev <jacobschwantes@outlook.com>
2022-07-15 05:37:35 -0500

    create initial blank pages

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next"
+
+
+const Dashboard: NextPage = () =>{
+    return (
+        <h1>dashboard</h1>
+    )
+}
+
+export default Dashboard
---
+++ src/pages/settings.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next"
+
+
+const Settings: NextPage = () =>{
+    return (
+        <h1>settings</h1>
+    )
+}
+
+export default Settings
