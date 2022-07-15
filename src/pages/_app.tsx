e8e6495d7d6f1d1c8a177916c6b907be9fba4eb4
skrt-dev <jacobschwantes@outlook.com>
2022-07-15 05:37:08 -0500

    add session check and page loading bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,45 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../common/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
+import firebaseApp from "../modules/auth/firebase/clientApp";
+import Auth from "../pages/auth";
+import { useRouter } from "next/router";
+import AppLayout from "@layouts/AppLayout";
+import { useEffect } from "react";
+import ProgressBar from "@badrap/bar-of-progress";
+const progress = new ProgressBar({
+  size: 2,
+  color: "#6366f1",
+  className: "bar-of-progress",
+  delay: 100,
+});
+const auth = getAuth(firebaseApp);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeStart", progress.start);
+    router.events.on("routeChangeError", progress.finish);
+    router.events.on("routeChangeComplete", progress.finish);
+  }, [router.events]);
+
+  if (user && !error) {
+    return (
+      <AppLayout>
+        <Component {...pageProps} />
+      </AppLayout>
+    );
+  } else if (!user && !loading) {
+    return <Auth />;
+  } else {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center  ">
+        <img src="/loading.svg"></img>
+      </div>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
