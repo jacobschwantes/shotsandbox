567f2e6266314384170e4a093d7ab38943b5941b
skrt-dev <jacobschwantes@outlook.com>
2022-07-19 06:42:54 -0500

    create modal component

--- src/common/components/index.ts
+++ src/common/components/index.ts
@@ -5,4 +5,5 @@ import Spinner from "./Spinner";
 import Card from "./Card";
 import Table from "./Table";
 import LineChart from "./LineChart";
-export { Header, Navigation, Notifications, Spinner, Card, Table, LineChart };
+import Modal from "./Modal";
+export { Header, Navigation, Notifications, Spinner, Card, Table, LineChart, Modal };
