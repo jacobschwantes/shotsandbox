6f719209397fd366ed6fb9ebad986d6f0989c026
skrt-dev <jacobschwantes@outlook.com>
2022-07-24 17:51:21 -0500

    update settings layout

--- src/common/layouts/SettingsLayout.tsx
+++ src/common/layouts/SettingsLayout.tsx
@@ -1,129 +1,71 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import { Header, Navigation } from "@components/index";
-
-import { Dialog } from "@headlessui/react";
-import {
-  BriefcaseIcon,
-  ChatIcon,
-  DocumentSearchIcon,
-  HomeIcon,
-  MenuAlt2Icon,
-  UsersIcon,
-} from "@heroicons/react/outline";
 import clsx from "clsx";
 import {
-  BookOpenIcon,
-  ChipIcon,
+  UserIcon,
   CreditCardIcon,
-  DownloadIcon,
-  MenuIcon,
-  XIcon,
-  CogIcon,
+  AdjustmentsIcon,
 } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
-const navigation = [
-  { name: "Pricing", href: "/pricing", icon: CreditCardIcon },
-  { name: "Download", href: "/download", icon: DownloadIcon },
-  { name: "Docs", href: "#", icon: BookOpenIcon },
-  { name: "App", href: "#", icon: ChipIcon },
-];
+import { ReactNode } from "react";
 
-const SettingsLayout: NextPage = ({ children }) => {
+interface SettingsLayoutProps {
+  children: ReactNode
+}
+const SettingsLayout: NextPage<SettingsLayoutProps> = ({ children }) => {
   const router = useRouter();
   const tabs = [
     {
-      name: "General",
-      href: "/settings",
-      current: router.asPath.endsWith("/settings"),
-    },
-    {
-      name: "Password",
-      href: "/settings/password",
-      current: router.asPath.includes("password"),
-    },
-    {
-      name: "Notifications",
-      href: "/settings/notifications",
-      current: router.asPath.includes("notifications"),
-    },
-    {
-      name: "Plan",
-      href: "/settings/plan",
-      current: router.asPath.includes("plan"),
+      name: "Account",
+      href: "/settings/account",
+      icon: UserIcon,
+      current: router.asPath.includes("settings/account"),
     },
     {
       name: "Billing",
       href: "/settings/billing",
-      current: router.asPath.includes("billing"),
+      icon: CreditCardIcon,
+      current: router.asPath.includes("settings/billing"),
     },
     {
-      name: "Team Members",
-      href: "/settings/team",
-      current: router.asPath.includes("team"),
+      name: "Preferences",
+      href: "/settings/preferences",
+      icon: AdjustmentsIcon,
+      current: router.asPath.includes("settings/preferences"),
     },
   ];
 
   return (
-    <div className=" flex flex-col md:px-8 xl:px-0 w-full">
-      <main className="flex-1">
-        <div className="relative  mx-auto md:px-8 xl:px-0">
-          <div className=" ">
-            <div className="px-4 sm:px-6 md:px-0">
-              <h1 className="text-3xl font-extrabold text-gray-900">
-                Settings
-              </h1>
-            </div>
-            <div className="px-4 sm:px-6 md:px-0">
-              <div className="py-3">
-                {/* Tabs */}
-                <div className="lg:hidden">
-                  <label htmlFor="selected-tab" className="sr-only">
-                    Select a tab
-                  </label>
-                  <select
-                    id="selected-tab"
-                    name="selected-tab"
-                    className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
-                    defaultValue={tabs.find((tab) => tab.current).name}
-                  >
-                    {tabs.map((tab) => (
-                      <option key={tab.name}>{tab.name}</option>
-                    ))}
-                  </select>
-                </div>
-                <div className="hidden lg:block">
-                  <div className="border-b border-gray-200">
-                    <nav className="-mb-px flex space-x-8">
-                      {tabs.map((tab) => (
-                        <Link
-                          key={tab.name}
-                          href={tab.href}
-                          
-                        >
-                            <a  className={clsx(
-                            tab.current
-                              ? "border-blue-500 text-blue-600"
-                              : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                            "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                          )}>
-
-                           
-                          {tab.name} </a>
-                        </Link>
-                      ))}
-                    </nav>
-                  </div>
-                </div>
-
-                {children}
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
+    <div className="space-y-4 ">
+      <div className="pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between w-full">
+        <h3 className="text-lg leading-6 font-medium text-gray-900">Settings</h3>
+      </div>
+      <div className="flex space-x-2 rounded-xl bg-gray-100 p-1 max-w-4xl ">
+        {tabs.map((tab) => {
+          return (
+            <Link key={tab.name} href={tab.href}>
+              <a
+                key={tab.name}
+                className={clsx(
+                  "flex w-full items-center justify-center rounded-lg py-2.5 text-center text-sm font-medium capitalize leading-5 ",
+                  "ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2",
+                  tab.current
+                    ? "bg-white text-blue-700 shadow"
+                    : "text-gray-700 hover:bg-white/[0.12] hover:text-gray-600"
+                )}
+              >
+                <h1 className="hidden sm:block">{tab.name}</h1>
+                <tab.icon className="h-5 sm:hidden" />
+              </a>
+            </Link>
+          );
+        })}
+      </div>
+      <div className="rounded-xl bg-white">{children}</div>
     </div>
   );
 };
+export default SettingsLayout
 
-export default SettingsLayout;
+
+
