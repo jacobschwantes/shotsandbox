50eda6d8ff7aad7e37ca18823b5b5e0313ac0fad
skrt-dev <jacobschwantes@outlook.com>
2022-07-24 17:50:41 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/settings',
+        destination: '/settings/account',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
