dcfd434756e38b865de0bd5a495a7692739a2d6d
skrt-dev <jacobschwantes@outlook.com>
2022-07-29 00:31:01 -0500

    remove toast options from notification

--- src/common/components/Notifications/Notifications.tsx
+++ src/common/components/Notifications/Notifications.tsx
@@ -71,13 +71,6 @@ export default function Notifications(props) {
         if (change.type === "added") {
           if (data.timestamp > start) {
             toast(data.message, {
-              position: "top-right",
-              autoClose: 5000,
-              hideProgressBar: false,
-              closeOnClick: true,
-              pauseOnHover: true,
-              draggable: true,
-              progress: undefined,
               });
           }
         }
