2a951a2af1bf04b70804e1787448038cdc635e20
skrt-dev <jacobschwantes@outlook.com>
2022-07-31 22:56:24 -0500

    error handling for expired token case

--- src/common/utils/middlewares.ts
+++ src/common/utils/middlewares.ts
@@ -1,17 +1,19 @@
-import { auth } from '@utils/firebase-admin';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { auth } from "@utils/firebase-admin";
+import { NextApiRequest, NextApiResponse } from "next";
 export function withAuth(handler: any) {
-    return async (req: NextApiRequest, res: NextApiResponse) => {
-        const authHeader = req.headers.authorization;
-      if (!authHeader) return res.status(401).end("Not authenticated");
-      const token = authHeader.split(' ')[1];
-  
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return res.status(401).end("Not authenticated");
+    const token = authHeader.split(" ")[1];
+    try {
       const { uid, email, email_verified } = await auth.verifyIdToken(token);
-      
       if (!uid || !email) {
-        return res.status(401).end("Not a valid user");
+        res.status(401).json({ message: "invalid user" });
+      } else {
+        return handler({ ...req, uid, email, email_verified }, res);
       }
-  
-      return handler({ ...req, uid, email, email_verified }, res);
-    };
-  }
+    } catch (e) {
+      res.status(401).json({ message: "error authenticating user" });
+    }
+  };
+}
