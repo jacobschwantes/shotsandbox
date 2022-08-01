f0e9a667e23aa1ff57075cf058fe07d3a7e19549
skrt-dev <jacobschwantes@outlook.com>
2022-07-31 22:55:45 -0500

    refactor header to render in component and styling

--- src/common/components/LineChart/LineChart.tsx
+++ src/common/components/LineChart/LineChart.tsx
@@ -1,38 +1,15 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import { DateTime } from "luxon";
+import clsx from "clsx";
 
 const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 export default function LineChart(props) {
   const options = {
-    chart: {
-      foreColor: props.dark ? "#f3f4f6" : "#111827",
-      toolbar: {
-        show: true,
-        offsetX: 0,
-        offsetY: 0,
-        tools: {
-          download: true,
-          selection: true,
-          zoom: true,
-          zoomin: true,
-          zoomout: true,
-          pan: true,
-          reset: true | '<img src="/static/icons/reset.png" width="20">',
-          customIcons: [],
-        },
-
-        autoSelected: "zoom",
-      },
-    },
-
-    xaxis: {
-      type: "category",
-    },
-
     options: {
       theme: {
-        mode: "light",
+        mode: "dark",
         palette: "palette1",
         monochrome: {
           enabled: false,
@@ -41,19 +18,26 @@ export default function LineChart(props) {
           shadeIntensity: 0.65,
         },
       },
+      
 
       chart: {
+        background: "transparent",
         zoom: {
           enabled: false,
         },
-        fontFamily:
-          'font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+       
+        toolbar: {
+          show: false,
+        },
+
+        fontFamily: 'font-family: Inter',
       },
       dataLabels: {
         enabled: false,
         style: {
           fontSize: "20px",
           fontWeight: "bold",
+          fontFamily: 'font-family: Inter',
         },
       },
       stroke: {
@@ -62,27 +46,6 @@ export default function LineChart(props) {
         lineCap: "butt",
         width: 3,
         dashArray: 0,
-      },
-      title: {
-        text: "Area Installed",
-        align: "left",
-        style: {
-          fontSize: "20px",
-          fontWeight: 600,
-          color: props.dark ? "#f4f4f5" : "#111827",
-        },
-      },
-      subtitle: {
-        text: "(+43%) than last year",
-        align: "left",
-        margin: 10,
-
-        floating: false,
-        style: {
-          fontSize: "16px",
-          fontWeight: "normal",
-          color: "#9699a2",
-        },
       },
 
       legend: {
@@ -97,45 +60,80 @@ export default function LineChart(props) {
       },
 
       grid: {
-        strokeDashArray: 4,
-        borderColor: props.dark ? "#27272a" : "#e5e7eb",
+        show: false,
+        strokeDashArray: 3,
+        borderColor: props.dark ? "#18181b" : "#e5e7eb",
       },
       yaxis: {
         labels: {
+          show: false,
           style: {
-            colors: props.dark ? "#f3f4f6" : "#9ca3af",
+            colors: props.dark ? "#71717a" : "#9ca3af",
             fontSize: "12px",
-
-            fontWeight: 400,
+            fontFamily: 'font-family: Inter',
+            fontWeight: 600,
           },
         },
       },
+      tooltip :{
+        marker: {
+          show: true
+        }
+      },
       xaxis: {
+
+        crosshairs: {
+          show: false
+        },
+        tooltip: {
+          enabled: false
+        },
+        type: "category",
         axisTicks: {
           show: false,
         },
+        tickAmount: 5,
         axisBorder: {
-          show: false,
+          show: true,
+          offsetY: 6,
+          color: "#18181b"
         },
         labels: {
-          style: {
-            colors: props.dark ? "#f3f4f6" : "#9ca3af",
-            fontSize: "12px",
 
-            fontWeight: 400,
+           formatter: function (value) {
+            
+            return DateTime.fromISO(value).toFormat('LLL d');
+          },
+          style: {
+            colors: props.dark ? "#71717a" : "#9ca3af",
+            fontSize: "12px",
+            fontFamily: 'font-family: Inter',
+            fontWeight: 600,
           },
         },
       },
     },
   };
+  const dataOptions = ['1w', '1m']
 
   return (
-    <div className="rounded-2xl  p-5 border  dark:border-zinc-900  border-gray-100 dark:bg-black">
+    <div className="rounded-2xl  p-5 border  dark:border-zinc-900  border-gray-100 dark:bg-black space-y-3">
+      <div className="flex justify-between items-start">
+        <span>
+          <h1 className="font-medium text-zinc-100 text-xl">Requests</h1>
+          <p className="text-zinc-400">{props.seriesOption === "1w" ? "Over last 7 days" : "Over last 30 days"}</p>
+        </span>
+        <div className="text-zinc-100 flex space-x-3 text-sm">
+          {dataOptions.map((option) => (
+            <button className={clsx("uppercase transition-colors duration-300 ", props.seriesOption === option ? "text-blue-500" : "text-zinc-500 hover:text-zinc-100")} onClick={() => props.setData(option)}>{option}</button>
+          ))}
+        </div>
+      </div>
       <Chart
         options={options.options}
         series={[
           {
-            name: "2022",
+            name: "Requests",
             data: props.data,
           },
         ]}
