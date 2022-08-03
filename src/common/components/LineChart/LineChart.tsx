52db44f0cdb278464c9c5b15edb25bd18a46c8d4
skrt-dev <jacobschwantes@outlook.com>
2022-08-03 00:27:50 -0500

    loading state

--- src/common/components/LineChart/LineChart.tsx
+++ src/common/components/LineChart/LineChart.tsx
@@ -18,26 +18,24 @@ export default function LineChart(props) {
           shadeIntensity: 0.65,
         },
       },
-      
-
       chart: {
         background: "transparent",
         zoom: {
           enabled: false,
         },
-       
+
         toolbar: {
           show: false,
         },
 
-        fontFamily: 'font-family: Inter',
+        fontFamily: "font-family: Inter",
       },
       dataLabels: {
         enabled: false,
         style: {
           fontSize: "20px",
           fontWeight: "bold",
-          fontFamily: 'font-family: Inter',
+          fontFamily: "font-family: Inter",
         },
       },
       stroke: {
@@ -47,7 +45,6 @@ export default function LineChart(props) {
         width: 3,
         dashArray: 0,
       },
-
       legend: {
         position: "top",
         horizontalAlign: "right",
@@ -58,7 +55,6 @@ export default function LineChart(props) {
           colors: props.dark ? "#f3f4f6" : "#111827",
         },
       },
-
       grid: {
         show: false,
         strokeDashArray: 3,
@@ -70,23 +66,22 @@ export default function LineChart(props) {
           style: {
             colors: props.dark ? "#71717a" : "#9ca3af",
             fontSize: "12px",
-            fontFamily: 'font-family: Inter',
+            fontFamily: "font-family: Inter",
             fontWeight: 600,
           },
         },
       },
-      tooltip :{
+      tooltip: {
         marker: {
-          show: true
-        }
+          show: true,
+        },
       },
       xaxis: {
-
         crosshairs: {
-          show: false
+          show: false,
         },
         tooltip: {
-          enabled: false
+          enabled: false,
         },
         type: "category",
         axisTicks: {
@@ -96,36 +91,103 @@ export default function LineChart(props) {
         axisBorder: {
           show: true,
           offsetY: 6,
-          color: "#18181b"
+          color: "#18181b",
         },
         labels: {
-
-           formatter: function (value) {
-            
-            return DateTime.fromISO(value).toFormat('LLL d');
+          formatter: function (value) {
+            return DateTime.fromISO(value).toFormat("LLL d");
           },
           style: {
             colors: props.dark ? "#71717a" : "#9ca3af",
             fontSize: "12px",
-            fontFamily: 'font-family: Inter',
+            fontFamily: "font-family: Inter",
             fontWeight: 600,
           },
         },
       },
     },
+  loadingOptions: {
+      colors: ["#27272a"],
+      fill: {
+        type: "solid",
+        colors: ["#18181b"],
+      },
+      chart: {
+        sparkline: {
+          enabled: true,
+        },
+        background: "transparent",
+        animations: {
+          enabled: false,
+        },
+        dynamicAnimation: {
+          enabled: false,
+        },
+      },
+      stroke: {
+        show: true,
+        curve: "smooth",
+        lineCap: "butt",
+        width: 1,
+        dashArray: 0,
+      },
+      tooltip: {
+        enabled: false,
+        marker: {
+          show: false,
+        },
+      },
+    },
   };
-  const dataOptions = ['1w', '1m']
 
-  return (
+  const dataOptions = ["1w", "1m"];
+
+  return props.isLoading ? (
+    <div className="relative overflow-hidden rounded-2xl border  dark:border-zinc-900  border-gray-100 dark:bg-black space-y-3 opacity-70">
+      <div className="space-y-3 p-5">
+        <p className="h-4 bg-zinc-900 w-60 rounded-full"></p>
+        <p className="h-3 bg-zinc-900 w-96 rounded-full"></p>
+      </div>
+      <div className="before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:border-t before:border-rose-100/10 before:bg-gradient-to-r before:from-transparent before:via-rose-100/10 before:to-transparent">
+        <div className=" ">
+          <Chart
+            options={options.loadingOptions}
+            series={[
+              {
+                name: "Requests",
+                data: [1.5, 3, 2, 3],
+              },
+            ]}
+            type="area"
+            height={450}
+          />
+        </div>
+      </div>
+    </div>
+  ) : (
     <div className="rounded-2xl  p-5 border  dark:border-zinc-900  border-gray-100 dark:bg-black space-y-3">
       <div className="flex justify-between items-start">
         <span>
           <h1 className="font-medium text-zinc-100 text-xl">Requests</h1>
-          <p className="text-zinc-400">{props.seriesOption === "1w" ? "Over last 7 days" : "Over last 30 days"}</p>
+          <p className="text-zinc-400">
+            {props.seriesOption === "1w"
+              ? "Over last 7 days"
+              : "Over last 30 days"}
+          </p>
         </span>
         <div className="text-zinc-100 flex space-x-3 text-sm">
           {dataOptions.map((option) => (
-            <button className={clsx("uppercase transition-colors duration-300 ", props.seriesOption === option ? "text-blue-500" : "text-zinc-500 hover:text-zinc-100")} onClick={() => props.setData(option)}>{option}</button>
+            <button
+              className={clsx(
+                "uppercase transition-colors duration-300 ",
+                props.seriesOption === option
+                  ? "text-blue-500"
+                  : "text-zinc-500 hover:text-zinc-100"
+              )}
+              onClick={() => props.setData(option)}
+            >
+              {option}
+            </button>
           ))}
         </div>
       </div>
