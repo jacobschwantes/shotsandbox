0551f0094304ff352938d23e7f997067f8ea6677
skrt-dev <jacobschwantes@outlook.com>
2022-08-11 06:59:16 -0500

    toolbar styling options

--- src/modules/editor/components/Toolbar.tsx
+++ src/modules/editor/components/Toolbar.tsx
@@ -1,43 +1,128 @@
+import clsx from "clsx";
 import { NextComponentType, NextPageContext } from "next";
 interface ToolbarProps {
-  type: "light" | "dark";
+  options: {
+    show: boolean;
+    dark: boolean;
+    opacity: number;
+    buttons: {
+      show: boolean;
+      dark: boolean;
+      solid: boolean;
+    };
+    searchBar: {
+      show: boolean;
+    };
+  };
 }
 
 const Toolbar: NextComponentType<NextPageContext, {}, ToolbarProps> = ({
-  type,
+  options: { dark, buttons, searchBar, opacity },
 }) => {
-  switch (type) {
-    case "light":
-      return (
-        <div className=" w-full rounded-t-lg bg-gray-200">
-          <div className="flex space-x-3 p-1.5 ">
-            <span className="p-1.5 bg-red-500 rounded-full"></span>
-            <span className="p-1.5 bg-yellow-500 rounded-full"></span>
-            <span className="p-1.5 bg-green-500 rounded-full"></span>
-          </div>
+  return (
+    <div className="w-full absolute top-0 z-10">
+      <div
+        style={{ opacity }}
+        className={clsx(
+          dark ? "bg-zinc-700" : "bg-zinc-200 ",
+          "relative w-full py-3 px-8"
+        )}
+      >
+        {searchBar.show && (
+          <div
+            className={clsx(
+              dark ? "bg-zinc-500" : "bg-zinc-400",
+              "absolute w-2/5 inset-0 m-auto h-5 rounded-md"
+            )}
+          ></div>
+        )}
+        <div className="flex space-x-3">
+          <span
+            className={clsx(
+              buttons.dark
+                ? "bg-zinc-500  border-zinc-500"
+                : "bg-red-500 border-red-500",
+              !buttons.solid && "bg-transparent",
+              "p-2 rounded-full border-2"
+            )}
+          ></span>
+          <span
+            className={clsx(
+              buttons.dark
+                ? "bg-zinc-500  border-zinc-500"
+                : "bg-yellow-500 border-yellow-500",
+              !buttons.solid && "bg-transparent",
+              "p-2 rounded-full border-2"
+            )}
+          ></span>
+          <span
+            className={clsx(
+              buttons.dark
+                ? "bg-zinc-500  border-zinc-500"
+                : "bg-green-500 border-green-500",
+              !buttons.solid && "bg-transparent",
+              "p-2 rounded-full border-2"
+            )}
+          ></span>
         </div>
-      );
-    case "dark":
-      return (
-        <span className=" w-full rounded-t-lg bg-gray-600">
-          <span className="flex space-x-3 p-1.5 ">
-            <span className="h-3 w-3 bg-red-500 rounded-full"></span>
-            <span className="h-3 w-3 bg-yellow-500 rounded-full"></span>
-            <span className="h-3 w-3 bg-green-500 rounded-full"></span>
-          </span>
-        </span>
-      );
-    default:
-      return (
-        <div className=" w-full rounded-t-lg bg-gray-200">
-          <div className="flex space-x-3 p-1.5 ">
-            <span className="p-1.5 bg-red-500 rounded-full"></span>
-            <span className="p-1.5 bg-yellow-500 rounded-full"></span>
-            <span className="p-1.5 bg-green-500 rounded-full"></span>
-          </div>
+      </div>
+    </div>
+  );
+};
+export const PreviewToolbar: NextComponentType<
+  NextPageContext,
+  {},
+  ToolbarProps
+> = ({ options: { dark, buttons, searchBar, opacity } }) => {
+  return (
+    <div className="w-full absolute  z-10 bg-transparent">
+      <div
+        style={{ opacity }}
+        className={clsx(
+          dark ? "bg-zinc-700" : "bg-zinc-200 ",
+          "relative w-full py-1 px-2"
+        )}
+      >
+        {searchBar.show && (
+          <div
+            className={clsx(searchBar.show && "hidden",
+              dark ? "bg-zinc-500" : "bg-zinc-400",
+              "absolute w-2/5 inset-0 m-auto h-1.5 rounded-md"
+            )}
+          ></div>
+        )}
+        <div className="flex space-x-1">
+          <span
+            className={clsx(
+              buttons.dark
+                ? "bg-zinc-500  border-zinc-500"
+                : "bg-red-500 border-red-500",
+              !buttons.solid && "bg-transparent",
+              "p-1 rounded-full border"
+            )}
+          ></span>
+          <span
+            className={clsx(
+              buttons.dark
+                ? "bg-zinc-500  border-zinc-500"
+                : "bg-yellow-500 border-yellow-500",
+              !buttons.solid && "bg-transparent",
+              "p-1 rounded-full border"
+            )}
+          ></span>
+          <span
+            className={clsx(
+              buttons.dark
+                ? "bg-zinc-500  border-zinc-500"
+                : "bg-green-500 border-green-500",
+              !buttons.solid && "bg-transparent",
+              "p-1 rounded-full border"
+            )}
+          ></span>
         </div>
-      );
-  }
+      </div>
+    </div>
+  );
 };
 
 export default Toolbar;
