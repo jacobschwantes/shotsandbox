f80ba6a526f91864da5d41235d6dbceabb853229
skrt-dev <jacobschwantes@outlook.com>
2022-08-11 06:56:40 -0500

    add toast container to editor page

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,10 +1,26 @@
 import type { NextPage } from "next";
 import Editor from "@modules/editor/Editor";
-
+import { ToastContainer } from "react-toastify";
 
 const Home: NextPage = () => {
-
- return <Editor/>
+  return (
+    <>
+      <ToastContainer
+        position="bottom-right"
+        className="text-sm"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        theme="colored"
+        pauseOnHover
+      />
+      <Editor />
+    </>
+  );
 };
 
 export default Home;
