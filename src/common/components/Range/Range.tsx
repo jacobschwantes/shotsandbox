df89d420e0e3803280c353daacedf39b49e17431
skrt-dev <jacobschwantes@outlook.com>
2022-08-16 04:03:22 -0500

    add range labels, styling

--- src/common/components/Range/Range.tsx
+++ src/common/components/Range/Range.tsx
@@ -1,10 +1,18 @@
+import clsx from "clsx";
 import { NextComponentType, NextPageContext } from "next";
+const labels = {
+  percent: 37,
+  degree: 186,
+};
 interface RangeProps {
   children: React.ReactNode;
   value: number;
   set: (newValue: number) => void;
   min?: number;
   max?: number;
+  step?: number;
+  showValues: boolean;
+  label?: "degree" | "percent";
 }
 const Range: NextComponentType<NextPageContext, {}, RangeProps> = ({
   value,
@@ -12,20 +20,39 @@ const Range: NextComponentType<NextPageContext, {}, RangeProps> = ({
   set,
   min = -1,
   max = 1,
+  showValues = true,
+  label,
+  step = 1,
 }) => (
   <div>
     <label htmlFor="customRange1" className=" font-medium">
       {children}
     </label>
     <input
-      className="appearance-none w-full h-1 p-0 focus:outline-none focus:ring-0 focus:shadow-none bg-blue-500 rounded-full slider"
+      className="appearance-none w-full h-[1px] p-0 focus:outline-none focus:ring-0 focus:shadow-none bg-blue-500 rounded-full slider"
       value={value}
       type="range"
       min={min}
       max={max}
-      step={0.01}
+      step={step}
       onChange={(e) => set(parseFloat(e.target.value))}
     />
+    {showValues && (
+      <div className={clsx(min < 0 && "mr-1", "flex justify-between text-zinc-300 text-xs mt-1")}>
+        <p>
+          {min}
+          {label && String.fromCharCode(labels[label])}
+        </p>
+        <p>
+          {value}
+          {label && String.fromCharCode(labels[label])}
+        </p>
+        <p>
+          {max}
+          {label && String.fromCharCode(labels[label])}
+        </p>
+      </div>
+    )}
   </div>
 );
-export default Range
+export default Range;
