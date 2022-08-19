e5c328fe1a1f7f62e0ca3800976b8cc417b20ca6
skrt-dev <jacobschwantes@outlook.com>
2022-08-18 22:15:23 -0500

    switch to use env variable as service account cred

--- src/modules/auth/firebase/admin.ts
+++ src/modules/auth/firebase/admin.ts
@@ -1,12 +1,14 @@
-  //lib/firebase-admin.js
+//lib/firebase-admin.js
 
-  import * as admin from 'firebase-admin';
-  var serviceAccount = require("dotted-saga-352400-firebase-adminsdk-w5bhq-529440095e.json");
-  if (!admin.apps.length) {
-    admin.initializeApp({ credential: admin.credential.cert(serviceAccount)});
-  }
+import * as admin from "firebase-admin";
+var serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
+);
+if (!admin.apps.length) {
+  admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+}
 
-  const firestore = admin.firestore();
-  const auth = admin.auth();
+const firestore = admin.firestore();
+const auth = admin.auth();
 
-  export { firestore, auth }
+export { firestore, auth };
