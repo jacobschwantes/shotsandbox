631af965221815d6cf42c78653897e4901afaf8d
skrt-dev <jacobschwantes@outlook.com>
2022-09-17 18:03:37 -0500

    feat: add sign out button to mobile menu

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -18,10 +18,12 @@ import {
 import { useRouter } from "next/router";
 import Link from "next/link";
 import clsx from "clsx";
-import logo from "../../../../public/logo.png"
-import logo_short from "../../../../public/logo_short.png"
+import logo from "../../../../public/logo.png";
+import logo_short from "../../../../public/logo_short.png";
 import Image from "next/future/image";
-
+import { signOut, getAuth } from "firebase/auth";
+import { firebaseApp } from "@modules/auth/firebase/client";
+const auth = getAuth(firebaseApp);
 export default function Header() {
   const router = useRouter();
   const navigation = [
@@ -89,17 +91,16 @@ export default function Header() {
                 </div>
               </div>
               <div className="sm:hidden">
-
-             
-              <Link href="/editor">
-                <a className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:ring-offset-black">
-                  Open Editor
-                  <PencilIcon className="h-4 ml-1"/>
-                </a>
-              </Link> </div>
+                <Link href="/editor">
+                  <a className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:ring-offset-black">
+                    Open Editor
+                    <PencilIcon className="h-4 ml-1" />
+                  </a>
+                </Link>{" "}
+              </div>
               <div className="flex items-center sm:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black dark:text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-black dark:text-white hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-600">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -129,10 +130,24 @@ export default function Header() {
                       className={"h-6 w-6 flex-shrink-0 transition-colors "}
                       aria-hidden="true"
                     />
-                    <span className={clsx(item.current ? "text-blue-500" : "text-white")}>{item.name}</span>
+                    <span
+                      className={clsx(
+                        item.current ? "text-blue-500" : "text-white"
+                      )}
+                    >
+                      {item.name}
+                    </span>
                   </button>
                 </Link>
               ))}
+              <div className="flex w-full space-x-3 p-2">
+                <Disclosure.Button
+                  onClick={() => signOut(auth)}
+                  className="block text-center w-full  rounded-md border border-transparent px-3 py-1.5  bg-blue-600 text-sm font-medium text-white shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-300"
+                >
+                  Log out
+                </Disclosure.Button>
+              </div>
             </div>
           </Disclosure.Panel>
         </>
