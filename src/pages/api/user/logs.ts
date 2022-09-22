b96e5d0a91ca93f5ce5adb380122ba28b0b22c51
skrt-dev <jacobschwantes@outlook.com>
2022-09-22 18:52:40 -0500

    style: remove whitespace, commented out blocks

--- src/pages/api/user/logs.ts
+++ src/pages/api/user/logs.ts
@@ -21,29 +21,6 @@ interface ApiRequest extends NextApiRequest {
   email: string;
   email_verified: boolean;
 }
-// async function getCount(docRef) {
-//   const querySnapshot = await docRef.collection("usage").get();
-//   const documents = querySnapshot.docs;
-//   let count = 0;
-//   for (const doc of documents) {
-//     count += doc.get("count");
-//   }
-//   return count;
-// }
-// async function aggregateUsage(docs) {
-//   const keys = await Promise.all(
-//     docs.map(async (doc) => {
-//       const data = doc.data();
-//       const key = doc.id;
-//       const count = await getCount(firestore.collection("API_KEYS").doc(key));
-//       return { ...data, key, usage: count };
-//     })
-//   );
-//   return keys
-// }
-
-// Use the query for pagination
-// ...
 async function getCount(docRef: FirebaseFirestore.DocumentReference) {
   const querySnapshot = await docRef.collection("usage").get();
   const documents = querySnapshot.docs;
@@ -64,13 +41,7 @@ async function handler(req: ApiRequest, res: NextApiResponse<Data>) {
       .orderBy("id", "desc")
       .startAt(startAt)
       .limit(parseInt(limit));
-
     const snapshot = await first.get();
-
-    // Construct a new query starting at this document.
-    // Note: this will not have the desired effect if multiple
-    // cities have the exact same population value.
-
     if (snapshot.empty) {
       res.status(404).json({ message: "no logs", logs: [], entries: logCount });
     } else {
