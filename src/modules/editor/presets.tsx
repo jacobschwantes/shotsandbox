ebf266bf6a6e824cf9108c6e666f73500c55287e
skrt-dev <jacobschwantes@outlook.com>
2022-10-23 21:49:05 -0500

    feat: new preset

--- src/modules/editor/presets.tsx
+++ src/modules/editor/presets.tsx
@@ -79,6 +79,90 @@ export const defaultConfig: Config = {
 };
 export const templates: Partial<Config>[] = [
   {
+    id: "config5",
+    preview: "preset_5.png",
+    name: "default",
+    size: {
+      scale: 88,
+      dimensions: {
+        aspectRatio: "1 / 1",
+        width: 1080,
+        height: 1080,
+      },
+    },
+    orientation: {
+      rotateX: 4,
+      rotateY: -29,
+      rotateZ: 12,
+      perspective: 976,
+    },
+    position: {
+      x: -12,
+      y: 18,
+    },
+    background: {
+      type: "gradient",
+      color: "#252525",
+      gradient: {
+        stops: [
+          {
+            color: "#e3bcf5",
+            id: 1,
+          },
+          {
+            color: "#b9dcf2",
+            id: 2,
+          },
+        ],
+        direction: 90,
+      },
+    },
+    shadow: {
+      color: "rgba(17, 12, 46, 0.2)",
+      type: "2xl",
+      previewSize: "0px 10px 20px",
+      size: "0px 100px 200px",
+    },
+    border: {
+      radius: 0.8,
+      width: 0,
+      color: "rgba(0, 0, 0, 1)",
+    },
+    header: {
+      show: false,
+      anchored: false,
+      align: "vertical",
+      content: {
+        title: "The best image editing tool for founders",
+        subtitle: "Turn boring screenshots into stunning graphics",
+        color: "rgba(255, 255, 255, 1)",
+        bold: true,
+        italic: false,
+        size: 3.5,
+        padding: 5,
+        translateX: 0,
+      },
+    },
+    frame: {
+      show: true,
+      dark: false,
+      opacity: 0.3,
+      buttons: {
+        show: true,
+        dark: true,
+        solid: true,
+      },
+      searchBar: {
+        show: true,
+      },
+    },
+    watermark: {
+      show: false,
+      placement: "bottom-right",
+      theme: "light",
+    },
+  },
+  {
     id: "config1",
     preview: "preset_1.png",
     name: "default",
